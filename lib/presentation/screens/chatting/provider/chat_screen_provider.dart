c6959210cfc5ab9a3021a16741ce9fa80390feea
phuc1st <tanphongtp7@gmail.com>
2025-04-16 20:54:41 +0700

    feat: add get message from server

--- lib/presentation/screens/chatting/provider/chat_screen_provider.dart
+++ lib/presentation/screens/chatting/provider/chat_screen_provider.dart
@@ -1,87 +1,18 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:learn_connect/config/app_config.dart';
-import 'package:socket_io_client/socket_io_client.dart' as IO;
-
-class Message {
-  final String senderId;
-  final String receiverId;
-  final String content;
-  final DateTime timestamp;
-
-  Message({
-    required this.senderId,
-    required this.receiverId,
-    required this.content,
-    required this.timestamp,
-  });
-
-  factory Message.fromJson(Map<String, dynamic> json) {
-    return Message(
-      senderId: json['senderId'],
-      receiverId: json['receiverId'],
-      content: json['content'],
-      timestamp: DateTime.tryParse(json['timestamp'] ?? '') ?? DateTime.now(),
-    );
-  }
-
-  Map<String, dynamic> toJson() => {
-    'senderId': senderId,
-    'receiverId': receiverId,
-    'content': content,
-    'timestamp': timestamp.toIso8601String(),
-  };
-}
-
-class SocketService {
-  late final IO.Socket socket;
-
-  SocketService() {
-    socket = IO.io(
-      AppConfig.socketServerUrl,
-      <String, dynamic>{
-        'transports': ['websocket'],
-        'autoConnect': false,
-      },
-    );
-
-    socket.connect();
-
-    socket.on('connect', (_) {
-      // Khi kết nối thành công, gửi event đăng ký (join) với senderId
-      socket.emit('join', {'userId': AppConfig.userId});
-    });
-  }
-
-  // Gửi tin nhắn đến một người nhận xác định
-  void sendMessage(String receiverId, String content) {
-    socket.emit('send-private-message', {
-      'senderId': AppConfig.userId,
-      'receiverId': receiverId,
-      'content': content,
-      'timestamp': DateTime.now().toIso8601String(),
-    });
-  }
-
-  // Lắng nghe sự kiện tin nhắn từ server
-  void listenToMessages(Function(Map<String, dynamic>) onMessageReceived) {
-    socket.on('receive-private-message', (data) {
-      onMessageReceived(Map<String, dynamic>.from(data));
-    });
-  }
-
-  void dispose() {
-    socket.dispose();
-  }
-}
+import 'package:learn_connect/data/models/message_model.dart';
+import 'package:learn_connect/services/message_service.dart';
+import 'package:learn_connect/services/socket_service.dart';
 
 class ChatNotifier extends StateNotifier<List<Message>> {
   final SocketService socketService;
   final String receivedId;
+  final MessageService messageService;
 
   ChatNotifier({
     required this.socketService,
     required this.receivedId,
+    required this.messageService
   }) : super([]) {
     _fetchMessages();
     // Lắng nghe tin nhắn từ server
@@ -99,8 +30,8 @@ class ChatNotifier extends StateNotifier<List<Message>> {
     });
   }
   Future<void> _fetchMessages() async {
-    /*final messages = await ChatRepository().getMessagesForChat(receivedId);
-    state = messages;*/
+    final messages = await messageService.fetchMessages(AppConfig.userId, this.receivedId);
+    state = messages;
   }
   // Hàm gửi tin nhắn (với nội dung truyền vào)
   void sendMessage(String content) {
@@ -125,6 +56,7 @@ final chatMessagesProvider = StateNotifierProvider.family<ChatNotifier, List<Mes
   final socketService = SocketService();
   return ChatNotifier(
     socketService: socketService,
-    receivedId: receivedId
+    receivedId: receivedId,
+    messageService: MessageService()
   );
 });
