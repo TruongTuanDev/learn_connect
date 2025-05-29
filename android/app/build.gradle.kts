7c85dd76f35733360a58926f7f8fdea4bed95246
thitamnguyen <tamn7161@gmail.com>
2025-05-29 09:48:23 +0700

    tam

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,5 @@
 plugins {
     id("com.android.application")
-
-    // START: FlutterFire Configuration
-    id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
-
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -12,13 +7,9 @@ plugins {
 
 android {
     namespace = "com.example.learn_connect"
-
-    compileSdk = 35
-    ndkVersion = "27.0.12077973"
-
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
+//    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -34,11 +25,7 @@ android {
         applicationId = "com.example.learn_connect"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-
-        minSdk = 23
-
         minSdk = flutter.minSdkVersion
-
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,4 +1,11 @@
-class AppConfig{
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AppConfig {
   static const String socketServerUrl = "http://localhost:8080";
   static String userId = "680b1d44203bdd4db4fc2777";
+
+  static String get baseUrl {
+    // Sử dụng flutter_dotenv
+    return dotenv.env['BASE_URL'] ?? 'http://fallback-url.com';
+  }
 }
